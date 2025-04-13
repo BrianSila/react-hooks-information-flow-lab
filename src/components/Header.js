9f6800158d02d18dfd6e6dedfaeedbedaa65b239
Brian <brian.sila1@student.moringaschool.com>
2025-04-13 14:29:41 +0300

    final commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header({ onDarkModeClick }) {
+  return (
+    <header>
+      <h1>Shopster</h1>
+      <button onClick={onDarkModeClick}>
+        Toggle Dark Mode
+      </button>
+    </header>
+  );
+}
+
+export default Header;
